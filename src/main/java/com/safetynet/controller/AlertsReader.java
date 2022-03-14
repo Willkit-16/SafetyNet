596e236f674b40a93b5e683278ef422f6aa6453c
Will S <ostielee16@gmail.com>
2022-03-14 10:32:06 +0100

    Reader JSON file

--- src/main/java/com/safetynet/controller/AlertsReader.java
+++ src/main/java/com/safetynet/controller/AlertsReader.java
@@ -9,7 +9,9 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AlertsReader {
 	@SuppressWarnings("unchecked")
 	public void readDataFromJson() throws FileNotFoundException, IOException {
@@ -19,15 +21,50 @@ public class AlertsReader {
 
 			JSONObject jsonObject = (JSONObject) obj;
 
-			JSONArray listOfData = (JSONArray) jsonObject.get("data");
+			JSONArray listOfPersons = (JSONArray) jsonObject.get("persons");
 
-			Iterator<JSONObject> iterator = listOfData.iterator();
+			Iterator<JSONObject> iterator = listOfPersons.iterator();
 
 			while (iterator.hasNext()) {
 				System.out.println(iterator.next());
 			}
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		try {
+			Object obj = parser.parse(new FileReader("src/main/resources/data.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			JSONArray listOfFirestation = (JSONArray) jsonObject.get("firestations");
+
+			Iterator<JSONObject> iterator = listOfFirestation.iterator();
+
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Object obj = parser.parse(new FileReader("src/main/resources/data.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			JSONArray listOfMedicalrecords = (JSONArray) jsonObject.get("medicalrecords");
+
+			Iterator<JSONObject> iterator = listOfMedicalrecords.iterator();
+
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/main/java/com/safetynet/webappAlerts/WebappAlertsApplication.java
+++ src/main/java/com/safetynet/webappAlerts/WebappAlertsApplication.java
@@ -1,13 +1,22 @@
 package com.safetynet.webappAlerts;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.safetynet.controller.AlertsReader;
 
 @SpringBootApplication
 public class WebappAlertsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		SpringApplication.run(WebappAlertsApplication.class, args);
+		AlertsReader ar = new AlertsReader();
+
+		ar.readDataFromJson();
+		System.out.println(ar);
 	}
 
 }
